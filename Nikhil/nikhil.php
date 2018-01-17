0679c3278dc6628ad2e99aec5217ef48d4ec1da9
Nikhil <nikhil.nikhil.muz@gmail.com>
2018-01-18 03:33:07 +0530

    Added Plugin

---
+++ Nikhil/nikhil.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Plugin Name:  Nikhil
+Plugin URI:   https://nikhilkumar.ga
+Description:  WordPress Plugin by Nikhil for TBS Planet
+Version:      1.0
+Author:       Nikhil Kumar
+Author URI:   https://nikhilkumar.ga
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  nikhil
+*/
+
+//get parent category
+$nikhilparcats = get_categories();
+$nikhilparcatid='';
+foreach($nikhilparcats as $cats){
+	if($cats->name =='Test2'){$nikhilparcatid=$cats->term_id;}
+}
+
+//get child category
+$nikhilchildcats = get_categories( array('parent' => $nikhilparcatid));
+
+//add URL hook
+add_action('parse_request', 'nikhilurlhandler');
+function nikhilurlhandler() {
+	global $nikhilchildcats;
+	foreach($nikhilchildcats as $cat){
+		//for Newspoint
+   if($_SERVER["REQUEST_URI"] == '/feed/newspoint/'.$cat->slug) {
+	  $posts=get_posts(array('numberposts'=>-1,'category'=>$cat->term_id,'orderby' => 'date','order' => 'DESC'));
+	  //page details
+	  echo '"tr": "'.sizeof($posts).'",'.PHP_EOL;
+	  echo '"pp": "'.sizeof($posts).'",'.PHP_EOL;
+	  echo '"tp": "1'.'",'.PHP_EOL;
+	  echo '"cp": "1"'.PHP_EOL;
+	  echo PHP_EOL;
+	  //gallery details
+	   echo '"upd": "'.$posts[0]->post_date.'",'.PHP_EOL;
+	   echo '"id": "'.$cat->term_id.'",'.PHP_EOL;
+	   echo '"h1": "'.$cat->name.'",'.PHP_EOL;
+	   echo '"dm": "tbsplanet.com",'.PHP_EOL;
+	   echo '"tn": "photo",'.PHP_EOL;
+	   echo '"m": "'.get_option('siteurl').$cat->slug.'"'.PHP_EOL;
+	   echo PHP_EOL;
+	  //post details
+	  foreach($posts as $post){
+		  echo '"id": "'.$post->ID.'",'.PHP_EOL;
+		  echo '"h1": "'.$post->post_title.'",'.PHP_EOL;
+		  echo '"dm": "tbsplanet.com",'.PHP_EOL;
+		  echo '"cap": "';
+		  echo the_post_thumbnail_caption($post->ID).'",'.PHP_EOL;
+		  echo '"wu": "'.wp_get_attachment_image_src(get_post_thumbnail_id($post->ID),'full',false)[0].'",'.PHP_EOL;
+		  echo '"m": "'.wp_get_attachment_image_src(get_post_thumbnail_id($post->ID),'full',false)[0].'",'.PHP_EOL;
+		  echo '"ag": "TBS Planet Comics",'.PHP_EOL;
+		  echo '"d1": "'.$post->post_date.'"'.PHP_EOL;
+		  echo PHP_EOL;
+	  }
+      exit();
+	}
+	}
+}
